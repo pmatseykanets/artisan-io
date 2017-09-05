47044f9c64ccfa495744093a7ef6228eb2af5564
Peter Matseykanets <pmatseykanets@gmail.com>
2017-09-05 07:54:51 -0400

    Implement a Service Provider

---
+++ src/ArtisanIoServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ArtisanIo;
+
+use ArtisanIo\Console\ImportDelimitedCommand;
+use Illuminate\Support\ServiceProvider;
+
+class ArtisanIoServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $commands = collect([
+            'command.artisan-io.import-delimited' => ImportDelimitedCommand::class,
+        ]);
+
+        $commands->each(function ($command, $abstract) {
+            $this->app->singleton($abstract, function () use ($command) {
+                return new $command;
+            });
+        });
+
+        $this->commands($commands->keys()->toArray());
+    }
+}
